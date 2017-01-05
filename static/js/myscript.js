f2dd0bcb60390a369b639ebf595abf013b998d11
spencerhooks <spencerhooks@gmail.com>
2017-01-05 09:45:39 -0800

    got flip clock working on settings.html

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -1,20 +1,18 @@
 $( "#my-button" ).click(function() {
   console.log($('#my-button').prop('value'));
   document.getElementById('my-button').innerHTML = 'Insert into butt'
->>>>>>> origin/master
 });
-
-var clock;
 
 $(document).ready(function() {
   var clock;
 
   clock = $('.clock').FlipClock({
         clockFace: 'TwelveHourClock',
-        }
+        showSeconds: false,
     });
 
-    // clock.setTime(220880);
+
     // clock.setCountdown(true);
     // clock.start();
+
 });
