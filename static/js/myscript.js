b50be24be242831f673937adcb3dc9698ac8b6d5
spencerhooks <spencerhooks@gmail.com>
2017-01-05 08:30:07 -0800

    Added flipclock

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -3,3 +3,19 @@ function change()
     if (this.value=="button") this.value = "new button";
     else this.value = "button";
 }
+
+var clock;
+
+$(document).ready(function() {
+  var clock;
+
+  clock = $('.clock').FlipClock({
+        clockFace: 'TwelveHourClock',
+        }
+    });
+
+    // clock.setTime(220880);
+    // clock.setCountdown(true);
+    // clock.start();
+
+});
